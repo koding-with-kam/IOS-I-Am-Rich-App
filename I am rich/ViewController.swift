b89b00e1ab7442db07bf5d2c9b97de54c1c04675
khayes <khayes@hothouseinc.com>
2021-12-08 15:39:43 -0500

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Kam Hayes on 12/8/21.
+//  Copyright © 2021 Kam Hayes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
